c379f6f6b10b0f3110f20307d2b31335c0f7e79c
Maryam Elhamidi <unicolmaryame@gmail.com>
2023-10-24 22:43:33 -0400

    Finalized Part 1 - with input validations

--- testing.py
+++ testing.py
@@ -3,47 +3,52 @@ from Module_A2 import Product
 def getProductinput():
 
     product_name = input("Enter Product Name: ")
-    print("Product Name: ", Product.getProductname())
 
-
-    sale_price = input("Enter Product Sale Price: ")
-    if type(sale_price) == float:
+    sale_price = float(input("Enter Product Sale Price: "))
+    if sale_price > 0:
         pass
     else:
-        print("Please Enter a valid Sale Price. Must be a float.")
-        sale_price = input("Enter Product Sale Price: ")        
+        print("Invalid input. Sale Price must be greater than 0, while being an integer.")
+        sale_price = float(input("Enter Product Sale Price: "))
 
-
-    manufacture_cost = input("Enter Product Manufacture Cost: ")
-    if type(manufacture_cost) == float:
+    manufacture_cost = float(input("Enter Product Manufacture Cost: "))
+    if manufacture_cost > 0:
         pass
     else:
-        print("Please Enter a Valid Manufacture Cost. Must be a float.")
-        manufacture_cost = input("Enter Product Manufacture Cost: ")    
+        print("Invalid input. Manufacturing Cost must be greater than 0, while being an integer.")
+        manufacture_cost = float(input("Enter Product Manufacture Cost: "))
+    
 
-
-    stock_level = input("Enter Stock Level: ")
-    if type(stock_level) == int:
+    stock_level = int(input("Enter Stock Level: "))
+    if stock_level > 0:
         pass
     else:
-        print("Please Enter a valid Stock Level. Must be an integer.")
-        stock_level = input("Enter Stock Level: ")    
-
-
-    monthly_units_manufactured = input("Enter Estimated Monthly Units Manufactured: ")
-    if type(monthly_units_manufactured) == int:
+        print("Invalid input. Manufacturing Cost must be greater than 0, while being an integer.")
+        stock_level = int(input("Enter Stock Level: "))
+    
+    
+    monthly_units_manufactured = int(input("Enter Estimated Monthly Units Manufactured: "))
+    if monthly_units_manufactured >= 0:
         pass
     else:
-        print("Please Enter a valid Estimate of Monthly Units Manufactured. Must be an integer.")
-        monthly_units_manufactured = input("Enter Estimated Monthly Units Manufactured: ")           
+        print("Please Enter a valid Estimate of Monthly Units Manufactured. Must be an integer greater than or equal to 0.")
+        monthly_units_manufactured = int(input("Enter Estimated Monthly Units Manufactured: "))           
 
 
-    product_code = int(input("Enter Product Code (1 to 1000): "))
-    if 1 <= product_code <= 1000:
-        print("Product code: ", product_code.getProductcode())
+    product_code = int(input("Enter Product Code (100 to 1000): "))
+    if 100 <= product_code <= 1000:
         pass
     else:
-        print("Invalid input. Product code must be between 1 and 1000, that is an integer.")
-        product_code = int(input("Enter Product Code (1 to 1000): "))
-#return product_code, product_name, sale_price, manufacture_cost, stock_level, monthly_units_manufactured
+        print("Invalid input. Product code must be between 100 and 1000.")
+        product_code = int(input("Enter Product Code (100 to 1000): "))
+
+    product = Product(product_code, product_name, sale_price, manufacture_cost, stock_level, monthly_units_manufactured)
+    print("Product Code: ", product.getProductcode())
+    print("Product Name: ", product.getProductname())
+    print("Sale Price: ", product.getSaleprice())
+    print("Manufacture Cost: ", product.getManufacturecost())
+    print("Stock Level:" , product.getStocklevel())
+    print("Estimated Monthly Units Manufactured:" , Product.getMonthlyunits())
+
+
 print(getProductinput())
