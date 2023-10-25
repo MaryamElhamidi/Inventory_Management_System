b299b4819ef82fe9ad386e92792d716b393f9e9e
Maryam Elhamidi <unicolmaryame@gmail.com>
2023-10-24 22:20:06 -0400

    editing

--- testing.py
+++ testing.py
@@ -1,13 +1,9 @@
 from Module_A2 import Product
 
-
 def getProductinput():
+
     product_name = input("Enter Product Name: ")
-    if product_name.isalpha():
-        pass
-    else:
-        print("Please enter a valid name. Must be a string")
-        product_name = input("Enter Product Name: ")
+    print("Product Name: ", Product.getProductname())
 
 
     sale_price = input("Enter Product Sale Price: ")
@@ -32,20 +28,22 @@ def getProductinput():
     else:
         print("Please Enter a valid Stock Level. Must be an integer.")
         stock_level = input("Enter Stock Level: ")    
-        
+
+
     monthly_units_manufactured = input("Enter Estimated Monthly Units Manufactured: ")
     if type(monthly_units_manufactured) == int:
         pass
     else:
         print("Please Enter a valid Estimate of Monthly Units Manufactured. Must be an integer.")
         monthly_units_manufactured = input("Enter Estimated Monthly Units Manufactured: ")           
-        
+
+
     product_code = int(input("Enter Product Code (1 to 1000): "))
     if 1 <= product_code <= 1000:
+        print("Product code: ", product_code.getProductcode())
         pass
     else:
         print("Invalid input. Product code must be between 1 and 1000, that is an integer.")
         product_code = int(input("Enter Product Code (1 to 1000): "))
-    
-        
 #return product_code, product_name, sale_price, manufacture_cost, stock_level, monthly_units_manufactured
+print(getProductinput())
